cbc059cf27d2829dc400ea29a1db28fd14abeef1
MichaelProjects <michael.lichtenecker@outlook.com>
2021-11-07 10:09:32 +0100

    added subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 mod cli;
 mod io;
+use std::fmt;
 
 use structopt::StructOpt;
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, PartialEq, StructOpt)]
 #[structopt(name = "Depploy", about = "Create docker image of cargo project with Depploy.")]
-pub struct Depploy{
-    command: String,
-    #[structopt(short = "o", long = "output")]
-    output: String
+struct Depploy{
+    #[structopt(subcommand)]
+    command: Command
 }
+
+#[derive(StructOpt)]
+enum Command{
+    run,
+    build,
+    push,
+}
+impl fmt::Debug for Command {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
 
 fn main(){
     let cli = Depploy::from_args();
+    println!("{:?}", cli);
 }
