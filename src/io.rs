e5db953484594a4de95ccb81f765aa0b7f92f314
Michael Lichtenecker <Michael.Lichtenecker@retarus.de>
2021-12-17 16:02:32 +0100

    fixed run command and dir flag

--- src/io.rs
+++ src/io.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, path::PathBuf};
 
 pub struct Config {
     pub version: String,
@@ -13,27 +13,31 @@ impl Config {
     }
 }
 
-pub fn match_config() -> String {
+pub fn match_config(dir: &PathBuf) -> String {
+    //!
+    //! has given filenames and if one of the files a in the specified directory, then it will return the name of the config file.
+    //! 
+    //! Example:
+    //! ```
+    //! match_config(&PathBuf::from("/home/user/project/"));
+    //! ```
     let config_names = vec!["Cargo.toml", "conf.toml"];
-    let mut found = String::new();
-    let current_dir = std::env::current_dir().unwrap();
-    for entry in fs::read_dir(current_dir).unwrap() {
+    for entry in fs::read_dir(dir).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
         let str_path = path.to_str().unwrap();
         for config_name in config_names.iter() {
             if str_path.contains(config_name) {
-                found = str_path.to_string();
-                return found;
+                return str_path.split("/").last().unwrap().to_string();
             }
         }
     }
     panic!("Could not find config")
 }
 
-pub fn load_project_file(filename: String) -> std::io::Result<String> {
-    let content = fs::read_to_string(filename);
-    content
+pub fn load_project_file(path: &PathBuf, filename: &String) -> std::io::Result<String> {
+    let filename = format!("{}/{}", path.to_str().expect("lul"), filename);
+    fs::read_to_string(filename)
 }
 
 pub fn get_info(config_data: String) -> Config {
@@ -42,7 +46,8 @@ pub fn get_info(config_data: String) -> Config {
         if line.contains("version") {
             content.version = parse_line(line);
             break;
-        } else if line.contains("name") {
+        }
+        if line.contains("name") {
             content.name = parse_line(line);
         }
     }
@@ -52,4 +57,13 @@ pub fn get_info(config_data: String) -> Config {
 pub fn parse_line(line: &str) -> String {
     let vec = line.split("=").collect::<Vec<&str>>();
     return vec[1].replace("\"", "").to_string();
+}
+
+pub fn build_dir(dir: &PathBuf) -> String {
+    let mut dir_str = dir.to_str().expect("Path");
+    if dir_str.ne(".") {
+        let a = format!("{}/.",dir_str );
+        return a
+    }
+    ".".to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@ mod conf;
 mod io;
 mod commands;
 
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
+
 use commands::Command;
 use crate::build::{build_image, create_tag, push_image};
 use crate::conf::read_depploy_conf;
-use crate::io::{get_info, load_project_file, match_config};
+use crate::io::{build_dir, get_info, load_project_file, match_config};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -25,17 +28,28 @@ pub struct Depploy {
 
 
 fn main() {
-    let depploy_dir = String::from("/etc/depploy/settings.toml");
+    let depploy_dir =  PathBuf::from_str("/etc/depploy").unwrap();
     let cli = Depploy::from_args();
 
     match &cli.cmd {
         Command::Run { dir, debug } => {
-            let depploy = read_depploy_conf(depploy_dir).unwrap();
-            let filename = match_config();
-            let config_data = load_project_file(filename);
-            let data = get_info(config_data.expect("Could not find config"));
+            
+            let build_dir = build_dir(dir);
+            let depploy = match read_depploy_conf(&depploy_dir){
+                Ok(depploy) => depploy,
+                Err(err) => panic!("{}", err),
+            };
+
+            let filename = match_config(dir);
+            let config_data = match load_project_file(&dir, &filename){
+                Ok(data) => data,
+                Err(err) => panic!("Error: {}", err),
+            };
+
+
+            let data = get_info(config_data);
             let tag = create_tag(&data, depploy.docker_registry);
-            build_image(&tag, &dir.to_str().unwrap().to_string());
+            build_image(&tag, build_dir.as_str());
             push_image(&tag);
         }
     }
