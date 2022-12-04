facc1b00e58a546dbdceef519452dc57eba03eb0
MichaelProjects <michael.lichtenecker@gmail.com>
2022-12-04 11:09:02 +0100

    update

--- test.sh
+++
@@ -1,5 +0,0 @@
-if [ $RUNNER_OS == "ubuntu" ]; then
-    tar -zcvf archive/depploy.linux-amd64.tar.gz target/release/depploy
-  else
-    tar -zcvf archive/depploy.darwin-amd64.tar.gz target/release/depploy
-  fi
